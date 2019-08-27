cbb061106b9c43c8f85eb47514d0928b5e310846
nityyyaaaa <30504469+nityyyaaaa@users.noreply.github.com>
2019-08-27 21:51:36 +0800

    Update week-2_TABA_HW.ml
    added convolutions

--- week2/week-2_TABA_HW.ml
+++ week2/week-2_TABA_HW.ml
@@ -19,6 +19,29 @@
 
 (* ********** Convolutions ********** *)
 
+let test_cnv1 candidate =
+  let b0 = (candidate [] [] = [])
+    and b1 = (candidate [1] [1] = [(1,1)])
+    and b2 = (candidate [1;2] [1;2] = [(1,2);(2,1)])
+    and b3 = (candidate [1;2;3] [1;2;3] = [(1,3);(2,2);(3,1)])
+    in b0 && b1 && b2 && b3;;
+  
+
+let cnv1 xs ys =
+  let rec continue a y ans =
+  match a,y with
+  | ah::at, y::ys -> continue at ys ((ah,y)::ans)
+  | [], [] -> ans
+    in
+ let rec walk x a =
+    match x with
+    | [] -> continue a ys []
+    | xh::xt -> walk xt (xh::a)
+ in walk xs [];;
+
+let () = assert (test_cnv1 cnv1);;
+
+
 (* ********** *)
 
 (* ********** List Reversal ********** *)
